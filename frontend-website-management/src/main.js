34ddd4c421fd318706504c1a8a31e832528112e7
iSpan03 <TW3555488@gmail.com>
2024-03-18 22:50:26 +0800

    前端修改
    修改key

--- frontend-website-management/src/main.js
+++ frontend-website-management/src/main.js
@@ -3,9 +3,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import CKEditor from '@ckeditor/ckeditor5-vue';
-app.provide('tinyApiKey', import.meta.env.VITE_API_TINYMCE_API_KEY)
-const app = createApp(App)
 
+const app = createApp(App)
+app.provide('tinyApiKey', import.meta.env.VITE_API_TINYMCE_API_KEY)
 app.use(createPinia())
 app.use(router)
 app.use(CKEditor)
